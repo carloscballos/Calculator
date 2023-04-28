08fca2870604073df44f09e4b0719b29959958c2
carloscballos <quiapi@gmail.com>
2023-04-28 12:08:13 -0500

    first commit
    add plus

---
+++ Form1.Designer.cs
@@ -0,0 +1,154 @@
+﻿namespace appDesktop
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.value1 = new System.Windows.Forms.TextBox();
+            this.value2 = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.Result = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Tahoma", 50F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(226, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(375, 81);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Calculadora";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(70, 173);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(31, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Valor";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(12, 90);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(796, 39);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Por ahora solo sumamos, pero pronto mucho mas...";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(204, 173);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(31, 13);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Valor";
+            // 
+            // value1
+            // 
+            this.value1.Location = new System.Drawing.Point(37, 189);
+            this.value1.Name = "value1";
+            this.value1.Size = new System.Drawing.Size(100, 20);
+            this.value1.TabIndex = 4;
+            // 
+            // value2
+            // 
+            this.value2.Location = new System.Drawing.Point(172, 189);
+            this.value2.Name = "value2";
+            this.value2.Size = new System.Drawing.Size(100, 20);
+            this.value2.TabIndex = 5;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(370, 173);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(55, 13);
+            this.label5.TabIndex = 6;
+            this.label5.Text = "Resultado";
+            // 
+            // Result
+            // 
+            this.Result.Location = new System.Drawing.Point(351, 189);
+            this.Result.Name = "Result";
+            this.Result.Size = new System.Drawing.Size(100, 20);
+            this.Result.TabIndex = 7;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(82, 215);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(140, 23);
+            this.button1.TabIndex = 8;
+            this.button1.Text = "Click para sumar";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.plusNumbers);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(812, 475);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.Result);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.value2);
+            this.Controls.Add(this.value1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox value1;
+        private System.Windows.Forms.TextBox value2;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox Result;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace appDesktop
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void plusNumbers(object sender, EventArgs e)
+        {
+            int valueOne;
+            int valueTwo;
+
+            if (!int.TryParse(value1.Text, out valueOne))
+            {
+                MessageBox.Show("El primer valor no es un número querida");
+                return;
+            }
+
+            if (!int.TryParse(value2.Text, out valueTwo))
+            {
+                MessageBox.Show("El segundo valor no es un número querida");
+                return;
+            }
+
+            int result = valueOne + valueTwo;
+            Result.Text = result.ToString();
+        }
+    }
+}
